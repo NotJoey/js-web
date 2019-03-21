12579b751fb234137ea2c812f5dd497bc5c062ee
Joey <joey.marien2000@gmail.com>
2019-03-21 16:18:03 +0100

    cookie clicker prototype

--- exercise-serie-2/exercise-3/script.js
+++ exercise-serie-2/exercise-3/script.js
@@ -4,7 +4,33 @@ console.log("This script is linked properly!")
 
 //Write your JS code here...
 let cookies = 0;
+let clicks = 1;
+let multiprice = 50;
+let autoprice = 100;
+document.getElementById("multibutton").style.visibility = "hidden";
+
 function addpoint() {
-cookies++;
-console.log(cookies);
+  for (i = 0; i < clicks; i++) {
+  cookies++;
+}
+refresh();
+}
+
+function refresh() {
+  document.getElementById("score").innerHTML = "Score: " + Math.round(cookies);
+  if (cookies >= multiprice)
+  {
+    document.getElementById("multibutton").style.visibility = "visible";
+
+  }
+  else {
+    document.getElementById("multibutton").style.visibility = "hidden";
+  }
+}
+
+function multiply() {
+  clicks = clicks*2;
+  cookies = cookies - multiprice;
+  multiprice = multiprice * 1.5;
+  refresh();
 }
